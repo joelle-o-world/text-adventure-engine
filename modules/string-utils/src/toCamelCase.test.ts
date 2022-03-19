cfcd5a1eb31ba519ee50ce6c4938e2a704193771
Joelle Plow <joelleplow@aol.com>
2022-03-19 20:48:02 +0000

    new module: string-utils !

---
+++ modules/string-utils/src/toCamelCase.test.ts
@@ -0,0 +1,14 @@
+import { toCamelCase, splitCamelCase } from "./toCamelCase";
+
+test("toCamelCase", () => {
+  expect(toCamelCase("a big rhino", "inA binBag")).toBe("ABigRhinoInABinBag");
+});
+
+describe("parseCamelCase", () => {
+  test.each([
+    ["TheBigBear", ["the", "big", "bear"]],
+    ["OoohHello", ["oooh", "hello"]],
+  ])('parseCamelCase("%s") = %o', (str: string, answer) => {
+    expect(splitCamelCase(str)).toStrictEqual(answer);
+  });
+});
