eeb4a535d66033d1786e07d7e707083c1de5843e
Joelle Plow <joelleplow@aol.com>
2022-03-19 16:12:59 +0000

    add pos-tagging module

---
+++ modules/pos-tagging/src/pos-tagging.test.ts
@@ -0,0 +1,23 @@
+import { getPosTags } from "./pos-tagging";
+
+test.todo("Write some part-of-speech tagging unit tests");
+describe("getPosTags()", () => {
+  test.each([
+    ["computer", ["noun"]],
+    ["fish", ["noun", "verb"]],
+    ["pope", ["noun"]],
+    ["seduce", ["verb"]],
+    ["seduces", []],
+    ["lived", []],
+    ["hoover", ["noun", "verb"]],
+    ["love", ["verb", "noun"]],
+    ["orange", ["adjective", "noun"]],
+    ["melon", ["noun"]],
+  ])("%s has POS tags: %j", async (word: string, expectedTags) => {
+    const tags = await getPosTags(word);
+    for (let tag of expectedTags) expect(tags).toContain(tag);
+
+    for (let tag of tags)
+      if (!expectedTags.includes(tag)) fail(`has extra tag: ${tag}`);
+  });
+});
