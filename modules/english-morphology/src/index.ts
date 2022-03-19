9f548f2fc328d79c04b771ca983ee210f492f7bc
Joelle Plow <joelleplow@aol.com>
2022-03-19 16:14:41 +0000

    (english-morphology): export deprecated MorphologyRelation type
    Just for now, until I find a better solution

--- modules/english-morphology/src/index.ts
+++ modules/english-morphology/src/index.ts
@@ -5,3 +5,20 @@ export {
   deconjugateConcise,
 } from "./conjugate";
 export { plural, singular } from "./plural";
+
+/** @deprecated */
+export type MorphologyRelation = {
+  form: string;
+  base: string;
+  baseForm: string;
+};
+
+/** @deprecated */
+export function isMorphologyRelation(x: any): x is MorphologyRelation {
+  return (
+    typeof x === "object" &&
+    typeof x.form === "string" &&
+    typeof x.base === "string" &&
+    typeof x.baseForm === "string"
+  );
+}
